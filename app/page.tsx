4663fe86afddf35eeb67d6e27a317582187be5d1
M.Usman <muxman30@gmail.com>
2024-09-11 22:41:55 +0500

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
     }
   }, []);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center justify-between ">
       <Hero />
       <Explore />
       <Favorite />
